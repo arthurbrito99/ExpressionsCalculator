aa12edb105faafaee247a214d62e1e5588e9a76e
arthurbrito99 <arthur.brito1999@outlook.com>
2023-06-02 19:32:44 -0300

    Corrigindo resultado da expressão no teste

--- unit_tests/test_expressao_complexa.py
+++ unit_tests/test_expressao_complexa.py
@@ -7,4 +7,4 @@ class ExpressaoComplexaTest(unittest.TestCase):
     def test_expressao_complexa(self):
         expressao = "5 * (4 + 2) - 8 / (1 + 3)"
         resultado = calcular(expressao)
-        self.assertEqual(resultado, 27)
+        self.assertEqual(resultado, 28)
